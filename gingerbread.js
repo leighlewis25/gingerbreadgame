96332d20b528101247b85c1e0205efc3c0a67b94
leighlewis25 <30633430+leighlewis25@users.noreply.github.com>
2017-12-07 15:45:18 -0800

    Create gingerbread.js

---
+++ gingerbread.js
@@ -0,0 +1,27 @@
+@import url('https://fonts.googleapis.com/css?family=Merienda');
+
+canvas {
+  border: 3px solid navy;
+  margin-top: 10px;
+}
+
+progress {
+  width: 300px;
+
+}
+p{
+  font-size: 20px;
+  font-family: 'Merienda', cursive;
+  color: #456F9A;
+}
+
+h1 {
+  font-size: 34px;
+  font-family: 'Merienda', cursive;
+  color: #9CBBBB;
+}
+
+body {
+  margin-top: 0;
+  padding-left: 20px;
+}
