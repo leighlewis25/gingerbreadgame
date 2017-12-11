378b4378826e3f2748b3e8f3bbb915e164574123
leighlewis25 <30633430+leighlewis25@users.noreply.github.com>
2017-12-10 20:47:05 -0800

    Update gingerbread.js

--- gingerbread.js
+++ gingerbread.js
@@ -1,4 +1,3 @@
-const backgroundSong = document.getElementById('backgroundMusic');
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const progressBar = document.querySelector("progress");
@@ -9,6 +8,7 @@ const PLAYER_STARTING_Y = 150;
 const PLAYER_WIDTH = 67;
 const PLAYER_HEIGHT = 55;
 const ENEMY_SOURCE = "https://i.imgur.com/YapzdhJ.png";
+const ENEMY_SPEED = Math.random()*(3-.75)+.75;
 const ENEMY_STARTING_X = 80;
 const ENEMY_STARTING_Y = 200;
 const ENEMY_WIDTH = 60;
@@ -17,13 +17,10 @@ const HEALTH_SOURCE = 'https://i.imgur.com/ThdI5fE.png';
 const POWER_UP_WIDTH = 50;
 const POWER_UP_HEIGHT = 50;
 const GAME_IS_OVER_Y = canvas.height-50;
-const MAX_ENEMY_SPEED = 3;
-const MIN_ENEMY_SPEED = .7;
 const GAME_IS_OVER_X = 30;
 let gameIsOver = false;
 let timer = 0;
 let score = 0;
-document.getElementById('score').innerHTML= score;
 const TIME_UNTIL_SPAWN = 500;
 let healthExists = false;
 
@@ -33,6 +30,9 @@ snowyImage.src = "https://i.imgur.com/2z59iGt.jpg";
 let gameOverImg = new Image();
 gameOverImg.src = "https://i.imgur.com/ITiY5vs.png";
 
+let santaHatImg = new Image();
+santaHatImg.src = 'https://i.imgur.com/ThdI5fE.png';
+
 
 function startGame() {
   if (progressBar.value === 0) {
@@ -40,7 +40,6 @@ function startGame() {
     timer = 0;
     score = 0;
     document.getElementById('score').innerHTML= score;
-    backgroundSong.play();
     gameIsOver = false;
     Object.assign(player, {x: canvas.width / 2, y: canvas.height / 2});
     requestAnimationFrame(drawScene);
@@ -59,9 +58,13 @@ function haveCollided(sprite1, sprite2) {
   );
 }
 
+function randomPlace(){
+  return Math.random()*canvas.width;
+}
+
 class Sprite {
   draw() {
-    var img = new Image();
+    let img = new Image();
     img.src = this.source;
     ctx.drawImage(img, this.x, this.y, this.imgWidth, this.imgHeight);
   }
@@ -119,6 +122,20 @@ let player = new Player(
   PLAYER_HEIGHT
 );
 
+class Health extends Sprite {
+  constructor(source, x, y, imgWidth, imgHeight) {
+    super();
+    Object.assign(this, {
+      source,
+      x,
+      y,
+      imgWidth,
+      imgHeight
+    });
+  }
+}
+
+
 class Enemy extends Sprite {
   constructor(source, x, y, speed, imgWidth, imgHeight) {
     super();
@@ -134,21 +151,8 @@ class Enemy extends Sprite {
 }
 
 let enemies = [
-  new Enemy(ENEMY_SOURCE, ENEMY_STARTING_X, ENEMY_STARTING_Y, randomSpeed(), ENEMY_WIDTH, ENEMY_HEIGHT)
+  new Enemy(ENEMY_SOURCE, ENEMY_STARTING_X, ENEMY_STARTING_Y, ENEMY_SPEED, ENEMY_WIDTH, ENEMY_HEIGHT)
 ];
-
-class Health extends Sprite {
-  constructor(source, x, y, imgWidth, imgHeight) {
-    super();
-    Object.assign(this, {
-      source,
-      x,
-      y,
-      imgWidth,
-      imgHeight
-    });
-  }
-}
 
 let newHealth = new Health(HEALTH_SOURCE,randomPlace(), randomPlace(), POWER_UP_WIDTH, POWER_UP_HEIGHT
 );
@@ -168,14 +172,6 @@ function updateMouse(event) {
   mouse.y = event.clientY - top;
 }
 
-function randomPlace(){
-  return Math.random()*canvas.width;
-}
-
-function randomSpeed() {
-  return Math.random()*(MAX_ENEMY_SPEED-MIN_ENEMY_SPEED)+MIN_ENEMY_SPEED;
-}
-
 function updateScene() {
   player.moveToward(mouse, player.speed);
   enemies.forEach(enemy => enemy.moveToward(player, enemy.speed));
@@ -192,8 +188,8 @@ function updateScene() {
     player.y = canvas.height - player.imgHeight;
   }
   if (timer % TIME_UNTIL_SPAWN === 0) {
-    enemies.push(new Enemy(ENEMY_SOURCE, ENEMY_STARTING_X, ENEMY_STARTING_Y, randomSpeed(), ENEMY_WIDTH, ENEMY_HEIGHT));
-    score += 1;
+    enemies.push(new Enemy(ENEMY_SOURCE, ENEMY_STARTING_X, ENEMY_STARTING_Y, ENEMY_SPEED, ENEMY_WIDTH, ENEMY_HEIGHT));
+    score += 2;
     document.getElementById('score').innerHTML= score;
     if (enemies.length%2 === 0) {
     healthExists = true;
@@ -203,7 +199,8 @@ function updateScene() {
     progressBar.value +=2;
     healthExists = false;
     }
-  
+
+
   enemies.forEach(enemy => {
     if (haveCollided(enemy, player)) {
       progressBar.value -= 1;
@@ -224,8 +221,6 @@ function updateScene() {
   function gameOver() {
     clearBackground();
     ctx.drawImage(gameOverImg, 30, 30);
-    backgroundSong.pause();
-    backgroundSong.currentTime=0;
     //http://www.powerpointhintergrund.com/ppt-image/game-over-png-1406.html
   }
 
@@ -237,10 +232,10 @@ function updateScene() {
   function drawScene() {
     clearBackground();
     player.draw();
+    enemies.forEach(enemy => enemy.draw());
     if (healthExists === true) {
       newHealth.draw();
     }
-    enemies.forEach(enemy => enemy.draw());
     if (gameIsOver) {
       gameOver();
       ctx.font = '30px Berkshire Swash, cursive';
@@ -253,5 +248,4 @@ function updateScene() {
   }
 
 canvas.addEventListener('click', startGame);
-backgroundSong.play();
 requestAnimationFrame(drawScene);
